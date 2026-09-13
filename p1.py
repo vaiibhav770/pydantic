7acac053f576d9840ea8f233414640ea71248b55
Vaiibhav <ranuc968@outlook.com>
2026-09-14 04:37:04 +0530

    Defined Emails

--- p1.py
+++ p1.py
@@ -1,17 +1,19 @@
-from pydantic import BaseModel
+from pydantic import BaseModel,EmailStr
 from typing import List, Dict, Optional
 
 class patient(BaseModel):
 
     name:str
     age:int
+    Email:EmailStr
     weight:float
-    married:Optional[bool]=None
+    married:Optional[bool]=False
     allergies:Optional[List[str]]=None
     contacts_details:Dict[str,str]
 
 def insert_patient_info(patient: patient):
     print(patient.name)
+    print(patient.Email)
     print(patient.age)
     print(patient.weight)
     print(patient.married)
@@ -20,7 +22,7 @@ def insert_patient_info(patient: patient):
     print('updated')
 
 
-patient_info = {'name':'dk','age':'22','weight':'67.0','married':'true','contacts_details':{'email':'abc@gmail.com','phone_no':'0987654321'}}
+patient_info = {'name':'dk','Email':'abc@gmail.com','age':'22','weight':'67.0','married':'true','contacts_details':{'phone_no':'0987654321'}}
 
 patient1 = patient(**patient_info)
 
