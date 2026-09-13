fe3dc061f38a8fabfb525dfe55e1da6b6bb0ab45
Vaiibhav <ranuc968@outlook.com>
2026-09-14 04:26:23 +0530

    optional fields

--- p1.py
+++ p1.py
@@ -1,13 +1,13 @@
 from pydantic import BaseModel
-from typing import List, Dict
+from typing import List, Dict, Optional
 
 class patient(BaseModel):
 
     name:str
     age:int
     weight:float
-    married:bool
-    allergies:List[str]
+    married:Optional[bool]=None
+    allergies:Optional[List[str]]=None
     contacts_details:Dict[str,str]
 
 def insert_patient_info(patient: patient):
@@ -20,7 +20,7 @@ def insert_patient_info(patient: patient):
     print('updated')
 
 
-patient_info = {'name':'dk','age':'22','weight':'67.0','married':'true','allergies':['pollen','sand','gass'],'contacts_details':{'email':'abc@gmail.com','phone_no':'0987654321'}}
+patient_info = {'name':'dk','age':'22','weight':'67.0','married':'true','contacts_details':{'email':'abc@gmail.com','phone_no':'0987654321'}}
 
 patient1 = patient(**patient_info)
 
