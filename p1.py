adcc63bd42dd996df0ad59f058836547b209dca8
Vaiibhav <ranuc968@outlook.com>
2026-09-14 04:52:37 +0530

    Url added

--- p1.py
+++ p1.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel,EmailStr
+from pydantic import BaseModel,EmailStr,AnyUrl
 from typing import List, Dict, Optional
 
 class patient(BaseModel):
@@ -7,6 +7,7 @@ class patient(BaseModel):
     age:int
     Email:EmailStr
     weight:float
+    linkdin_url:AnyUrl
     married:Optional[bool]=False
     allergies:Optional[List[str]]=None
     contacts_details:Dict[str,str]
@@ -16,13 +17,14 @@ def insert_patient_info(patient: patient):
     print(patient.Email)
     print(patient.age)
     print(patient.weight)
+    print(patient.linkdin_url)
     print(patient.married)
     print(patient.allergies)
     print(patient.contacts_details)
     print('updated')
 
 
-patient_info = {'name':'dk','Email':'abc@gmail.com','age':'22','weight':'67.0','married':'true','contacts_details':{'phone_no':'0987654321'}}
+patient_info = {'name':'dk','Email':'abc@gmail.com','age':'22','weight':'67.0','linkdin_url':'https://linkdin.com/123','married':'true','contacts_details':{'phone_no':'0987654321'}}
 
 patient1 = patient(**patient_info)
 
