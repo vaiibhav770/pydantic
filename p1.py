6174239f92d294b639440be745acce3b26a493a8
Vaiibhav <ranuc968@outlook.com>
2026-09-15 04:49:54 +0530

    strict function in annotated

--- p1.py
+++ p1.py
@@ -6,7 +6,7 @@ class patient(BaseModel):
     name:Annotated[str,Field(max_length=50, title='Name of the patient',description='Give the name of the patient in less than 50 words',examples=['nitish','amit'])]
     age:int=Field(gt=0,lt=120)
     Email:EmailStr
-    weight:float=Field(gt=0)
+    weight:Annotated[float,Field(gt=0,strict=True)]
     linkdin_url:AnyUrl
     married:Annotated[bool,Field(default=None,description='is the patient married or not?')]
     allergies:Optional[List[str]]=Field(default=None,max_length=5)
@@ -24,7 +24,7 @@ def insert_patient_info(patient: patient):
     print('updated')
 
 
-patient_info = {'name':'dk','Email':'abc@gmail.com','age':'22','weight':'67.0','linkdin_url':'https://linkdin.com/123','married':'true','contacts_details':{'phone_no':'0987654321'}}
+patient_info = {'name':'dk','Email':'abc@gmail.com','age':'22','weight':67.0,'linkdin_url':'https://linkdin.com/123','married':'true','contacts_details':{'phone_no':'0987654321'}}
 
 patient1 = patient(**patient_info)
 
